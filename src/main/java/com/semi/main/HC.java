ecb6ec72a3c84f5d4e77bd64d6ca8d9dd304a122
seungbin <tmdqls511@gmail.com>
2023-12-07 11:01:23 +0900

    test

--- src/main/java/com/semi/main/HC.java
+++ src/main/java/com/semi/main/HC.java
@@ -12,6 +12,10 @@ public class HC extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("index.jsp");
+		
+		
+		
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
