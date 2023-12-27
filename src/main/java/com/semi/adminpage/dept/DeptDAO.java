c67e5162a499fc4937cb1e117c2b5613e7775d26
niav00 <vain94s@gmail.com>
2023-12-28 06:17:40 +0900

    부서 일관 운송부 추가

--- src/main/java/com/semi/adminpage/dept/DeptDAO.java
+++ src/main/java/com/semi/adminpage/dept/DeptDAO.java
@@ -82,16 +82,72 @@ public class DeptDAO {
 
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				contractDept.setC_deptno(rs.getInt("c_deptno"));
-				contractDept.setC_dept(rs.getString("c_dept"));
-				contractDept.setC_count(rs.getInt("c_count"));
-				contractDept.setC_contract_count(rs.getInt("c_contract_count"));
-				contractDept.setC_contract_items(rs.getInt("c_contract_items"));
-				contractDept.setC_total_price(rs.getInt("c_total_price"));
-				contractDept.setC_contract_completed(rs.getInt("c_contract_completed"));
-				contractDept.setC_awaiting_stock(rs.getInt("c_awaiting_stock"));
+				contractDept.setC_deptno(rs.getInt(1));
+				contractDept.setC_dept(rs.getString(2));
+				contractDept.setC_count(rs.getInt(3));
+				contractDept.setC_contract_count(rs.getInt(4));
+				contractDept.setC_contract_items(rs.getInt(5));
+				contractDept.setC_total_price(rs.getInt(6));
+				contractDept.setC_contract_completed(rs.getInt(7));
+				contractDept.setC_awaiting_stock(rs.getInt(8));
 
 				request.setAttribute(attributeName, contractDept);
+				
+				System.out.println("getContractDeptInfo : " + attributeName);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("error", "DBFail");
+		} finally {
+			DBManger.close(con, pstmt, rs);
+		}
+	}
+	
+	public static void getDistributionDeptInfo(HttpServletRequest request) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		DistributionDeptDTO distributionDept = new DistributionDeptDTO();
+		String sql = "SELECT\r\n"
+				+ "    d.d_deptno,\r\n"
+				+ "    d.d_dept,\r\n"
+				+ "    (SELECT COUNT(*) FROM employee e\r\n"
+				+ "     JOIN dept d ON e.e_deptno = d.d_deptno\r\n"
+				+ "     WHERE d.d_deptno = 201) AS d_count,\r\n"
+				+ "    (SELECT COUNT(*) FROM contract WHERE c_status = 1) AS d_waiting,\r\n"
+				+ "    (SELECT COUNT(*) FROM contract WHERE c_status = 2) AS d_allocated,\r\n"
+				+ "    (SELECT COUNT(*) FROM contract WHERE c_status = 2 AND TO_CHAR(c_delivery_date, 'yyyy-mm') = ?) AS d_allocatedThisMonth,\r\n"
+				+ "    (SELECT COUNT(*) FROM contract WHERE (c_status = 3 OR c_status=4) AND TO_CHAR(c_delivery_date, 'yyyy-mm') = ?) AS d_completedThisMonth,\r\n"
+				+ "    (SELECT count(*) FROM contract WHERE c_status < 3 AND (TRUNC(c_due_date) < TRUNC(SYSDATE) OR TRUNC(c_delivery_date) < TRUNC(SYSDATE))) as d_expired,\r\n"
+				+ "    (SELECT count(*) FROM contract WHERE c_status < 3 AND (TRUNC(c_due_date) = TRUNC(SYSDATE) OR TRUNC(c_delivery_date) = TRUNC(SYSDATE))) as d_dueDate,\r\n"
+				+ "    (SELECT count(*) FROM contract WHERE TRUNC(c_delivery_date) = TRUNC(SYSDATE)) as d_todayDelivery,\r\n"
+				+ "    (SELECT count(*) FROM contract WHERE (c_status = 3 OR c_status = 4) AND TRUNC(c_delivery_date) = TRUNC(SYSDATE)) as d_todayCompleted\r\n"
+				+ "FROM dept d\r\n"
+				+ "WHERE d.d_deptno = 201";
+		String currentYearMonth = AdminUtils.getCurrentYearMonth();
+
+		try {
+			con = DBManger.connect();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, currentYearMonth);
+			pstmt.setString(2, currentYearMonth);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				distributionDept.setD_deptno(rs.getInt(1));
+				distributionDept.setD_dept(rs.getString(2));
+				distributionDept.setD_count(rs.getInt(3));
+				distributionDept.setD_waiting(4);
+				distributionDept.setD_allocated(5);
+				distributionDept.setD_allocatedThisMonth(6);
+				distributionDept.setD_completedThisMonth(7);
+				distributionDept.setD_expired(rs.getInt(8));
+				distributionDept.setD_dueDate(rs.getInt(9));
+				distributionDept.setD_todayDelivery(10);
+				distributionDept.setD_todayCompleted(11);
+				
+				request.setAttribute("distributionDept", distributionDept);
 			}
 
 		} catch (Exception e) {
--- src/main/webapp/sjh/admin/dept_info.jsp
+++ src/main/webapp/sjh/admin/dept_info.jsp
@@ -28,7 +28,22 @@
 현월 물품건수 : ${salesDept.c_contract_items }<br>
 현월 계약 액수 : ${salesDept.c_total_price } <br>
 현월 계약 완료수 : ${salesDept.c_contract_completed }<br>
-입고 대기 건수 : ${salesDept. c_awaiting_stock } <br>
+입고 대기 건수 : ${salesDept.c_awaiting_stock } <br>
+</div>
+<br>
+<div>
+부서번호: ${distributionDept.d_deptno } <br>
+부서이름: ${distributionDept.d_dept } <br>
+인원수: ${distributionDept.d_count } <br>
+현월: ${currentYearMonth } <br>
+배차 대기: ${distributionDept.d_waiting } <br>
+배차 완료 : ${distributionDept.d_allocated }<br>
+현월 배차 완료 : ${distributionDept.d_allocatedThisMonth } <br>
+현월 배송 완료 : ${distributionDept.d_completedThisMonth }<br>
+만기된 운송 : ${distributionDept.d_expired } <br>
+만기일 운송 : ${distributionDept.d_dueDate } <br>
+당일 운송 : ${distributionDept.d_todayDelivery } <br>
+당일 운송 완료 : ${distributionDept.d_todayCompleted } <br>
 </div>
 </body>
 </html>
