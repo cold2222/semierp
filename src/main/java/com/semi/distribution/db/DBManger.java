815c4c82117af6999995f536cdb52a66b7104c59
sung joo kim <tjdwn49@gmail.com>
2024-01-04 18:27:35 +0900

    db원상복귀

--- src/main/java/com/semi/distribution/db/DBManger.java
+++ src/main/java/com/semi/distribution/db/DBManger.java
@@ -7,40 +7,29 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-
 public class DBManger {
-	private static BasicDataSource dataSource;
+	
 
-// 클래스 초기화 블록에서 BasicDataSource 초기화
-static {
-	dataSource = new BasicDataSource();
-	dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
-	dataSource.setUrl(
-			"jdbc:oracle:thin:@db202204301707_medium?TNS_ADMIN=/Users/sungjookim/Desktop/Ksj/Wallet_DB202204301707");
-	dataSource.setUsername("FORMAC");
-	dataSource.setPassword("Soldesk802!!");
-}
-
-// 데이터베이스 연결을 가져오는 메서드
-public static Connection connect() throws SQLException {
-	return dataSource.getConnection();
-}
-
-// 데이터베이스 자원 반환 메서드
-public static void close(Connection connection, PreparedStatement pstmt, ResultSet rs) {
-	try {
-		if (rs != null) {
-			rs.close();
-		}
-		if (pstmt != null) {
-			pstmt.close();
-		}
-		if (connection != null) {
-			connection.close();
-		}
-	} catch (SQLException e) {
-		e.printStackTrace();
+	public static Connection connect() throws SQLException, ClassNotFoundException {
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		return DriverManager.getConnection(url,"c##ysb836","tmdqls510");
 	}
-}
+
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			if(rs != null) {
+				rs.close();
+			}
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
 }
