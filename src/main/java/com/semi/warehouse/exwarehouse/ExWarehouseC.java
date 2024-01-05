abe7189e0695862ab4b7d8f9acfe2d9c43d16b71
sung joo kim <tjdwn49@gmail.com>
2024-01-05 18:23:34 +0900

    일단 권한주기 1개 시도 후 커밋

--- src/main/java/com/semi/warehouse/exwarehouse/ExWarehouseC.java
+++ src/main/java/com/semi/warehouse/exwarehouse/ExWarehouseC.java
@@ -7,19 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.semi.login.EmployeeDAO;
+
 
 @WebServlet("/ExWarehouseC")
 public class ExWarehouseC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ExWarehouseDAO.getEwdao().getExAll(request);
-		ExWarehouseDAO.getEwdao().paging(1, request);
-		
-		request.setAttribute("sidebar","sj/sidebar.jsp");
-        request.setAttribute("contentPage","sj/warehouse/ex_warehouse/ex_warehouse_interface.jsp");
-        request.setAttribute("selectedHeader", "stock");
-        request.getRequestDispatcher("index2.jsp").forward(request, response);
-		
-	}
+		 if (EmployeeDAO.getEmployeeManager().loginCheck(request)) {
+	            // 권한 확인 일단은 202만 들어가게 설정해놓음
+	            if (EmployeeDAO.getEmployeeManager().authorization(request, 202)) {
+	                // ExWarehouseDAO에서의 로직 수행
+	                ExWarehouseDAO.getEwdao().getExAll(request);
+	                ExWarehouseDAO.getEwdao().paging(1, request);
+
+	                request.setAttribute("sidebar", "sj/sidebar.jsp");
+	                request.setAttribute("contentPage", "sj/warehouse/ex_warehouse/ex_warehouse_interface.jsp");
+	                request.setAttribute("selectedHeader", "stock");
+	                request.getRequestDispatcher("index2.jsp").forward(request, response);
+	            } else {
+	                // 권한이 없는 경우에 대한 처리
+	            	request.getRequestDispatcher("HC").forward(request, response);
+	            }
+	        } else {
+	            // 로그인되지 않은 경우 로그인 페이지로 리다이렉트 
+	            response.sendRedirect("LoginC");
+	        }
+	    }
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
